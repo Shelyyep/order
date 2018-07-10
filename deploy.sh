95168b75d8796dd8a4e59144a060c753f4dabb91
zhengshukun <zhengshukun@huya.com>
2018-07-10 13:52:10 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -31,8 +31,8 @@ rm -f $TOMCAT_APP_PATH/webapps/order.war
 # 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
-cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+#cd $TOMCAT_APP_PATH/webapps/
+#mv order.war ROOT.war
 
 
 # 停tomcat
